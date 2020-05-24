6e45eaff1d23452ce40b066a8c680a8af3249708
ssannkkallpp <ssannkkallpp@gmail.com>
2020-05-24 12:30:03 +0530

    Update epydemic.py
    Scaled average results

--- epydemic.py
+++ epydemic.py
@@ -3,7 +3,9 @@ from random import choice
 from random import randint
 import random
 from epydemic import *
-
+from numpy import *
+import numpy
+import epydemic
 
 
 class SIR(CompartmentedModel):
@@ -40,26 +42,32 @@ class SIR(CompartmentedModel):
     def remove(self, t, n):
         self.changeCompartment(n, self.REMOVED)
 
-def rand_transmission():
-    # function to vary transmission probability
-    return 0.01
-
 def main():
     param = dict()
-    param[SIR.P_INFECT] = 0.000025 # initial infected population based on karnataka data
+    param[SIR.P_INFECT] = 0.02 # initial infected population based on karnataka data
     param[SIR.P_REMOVE] = 0.5
-    param[SIR.P_INFECTED] = rand_transmission()
+    param[SIR.P_INFECTED] = 0.01
 
     n = 10 ** 3  # total population
     p = 0.05
     G = nx.erdos_renyi_graph(n, p)
 
     model = SIR() # initialize the simple model
-    sim = SynchronousDynamics(model, G) #set up stochastic spread model
+    sim = StochasticDynamics(model, G) #set up stochastic spread model
     result = sim.set(param).run() #run on our simple parameters
+    data = []
 
-    print(result)
+    i = 0.0001
+    while i <= 0.1:
+        param[SIR.P_INFECT] = i
 
+        average = 0
+        for j in range(100):
+            res = sim.set(param).run()
+            average += (n - res['results']['S'])
+        data.append(average/100.0)
+        i *= 10
 
+    return data
 
-main()
+print(main())
