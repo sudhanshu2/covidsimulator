b4188421df8f708b49ad0591e940a9ccdac23ca8
Lint Action <lint-action@samuelmeuli.com>
2021-08-02 02:37:33 +0000

    Fix code style issues with Black

--- data.py
+++ data.py
@@ -1,22 +1,31 @@
-# File works as intended but needs to be unit tested. 
+# File works as intended but needs to be unit tested.
 
 from unittest.main import TestProgram
 from numpy.lib.stride_tricks import DummyArray
 import pandas as pd
-import numpy as np 
-import json 
-#from pathlib import Path
+import numpy as np
+import json
+
+# from pathlib import Path
 
 
 # Timeframe for the data read is initiated from Jan 22, 2021 : 22/01/2020
-_COVID_CASES_OVER_TIME_DATA = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
-_COVID_DEATHS_OVER_TIME_DATA = 'https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
-_COVID_RECOVERED_OVER_TIME_DATA = 'https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv'
+_COVID_CASES_OVER_TIME_DATA = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+_COVID_DEATHS_OVER_TIME_DATA = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+_COVID_RECOVERED_OVER_TIME_DATA = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
 
 
 _DATA_PATH = "data/"
 
-_FTypes = ["cumulative_total", "cumulative_death", "cumulative_recovered", "incident_total", "incident_death", "incident_recovered"]
+_FTypes = [
+    "cumulative_total",
+    "cumulative_death",
+    "cumulative_recovered",
+    "incident_total",
+    "incident_death",
+    "incident_recovered",
+]
+
 
 class NpEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -30,50 +39,51 @@ class NpEncoder(json.JSONEncoder):
             return super(NpEncoder, self).default(obj)
 
 
-
 def get_incident_cases(array):
     n = len(array)
-    if n == 0: raise ValueError("Empty list passed to get incident cases from, check fetch_data()\n")
+    if n == 0:
+        raise ValueError(
+            "Empty list passed to get incident cases from, check fetch_data()\n"
+        )
     res = [array[0]]
     for i in range(1, len(array)):
-        res.append(array[i] - array[i-1])
+        res.append(array[i] - array[i - 1])
 
     return res
 
 
-
 def fetch_data():
 
-    if (_COVID_CASES_OVER_TIME_DATA 
-    or _COVID_DEATHS_OVER_TIME_DATA 
-    or _COVID_RECOVERED_OVER_TIME_DATA) is None:
-        raise AssertionError("Data links to online csv files not correctly configured\n")
-
+    if (
+        _COVID_CASES_OVER_TIME_DATA
+        or _COVID_DEATHS_OVER_TIME_DATA
+        or _COVID_RECOVERED_OVER_TIME_DATA
+    ) is None:
+        raise AssertionError(
+            "Data links to online csv files not correctly configured\n"
+        )
 
     print("Loading data from online repo...\n")
     confirmed_df = pd.read_csv(_COVID_CASES_OVER_TIME_DATA)
     recovered_df = pd.read_csv(_COVID_CASES_OVER_TIME_DATA)
     death_df = pd.read_csv(_COVID_CASES_OVER_TIME_DATA)
-    
-    countries = confirmed_df['Country/Region'].unique()
 
-    confirmed_df = confirmed_df.drop(columns=['Province/State','Lat', 'Long'])
-    confirmed_df = confirmed_df.groupby('Country/Region').agg('sum')
+    countries = confirmed_df["Country/Region"].unique()
 
+    confirmed_df = confirmed_df.drop(columns=["Province/State", "Lat", "Long"])
+    confirmed_df = confirmed_df.groupby("Country/Region").agg("sum")
 
+    recovered_df = recovered_df.drop(columns=["Province/State", "Lat", "Long"])
+    recovered_df = recovered_df.groupby("Country/Region").agg("sum")
 
-    recovered_df = recovered_df.drop(columns=['Province/State','Lat', 'Long'])
-    recovered_df = recovered_df.groupby('Country/Region').agg('sum')
-
-
-    death_df = death_df.drop(columns=['Province/State','Lat', 'Long'])
-    death_df = death_df.groupby('Country/Region').agg('sum')
+    death_df = death_df.drop(columns=["Province/State", "Lat", "Long"])
+    death_df = death_df.groupby("Country/Region").agg("sum")
 
     print("Extracted data, now saving to dataset...\n")
 
     num_countries = len(countries)
     for c in range(num_countries):
-        country_name = confirmed_df.iloc[c].name 
+        country_name = confirmed_df.iloc[c].name
         data_table = []
         data_table.append(list(confirmed_df.iloc[c]))
         data_table.append(list(death_df.iloc[c]))
@@ -89,10 +99,7 @@ def fetch_data():
 
     print("Done loading and saving data set\n")
     return []
-    
-
-    
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     fetch_data()
