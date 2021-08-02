370ea022f0aacc5410c7a7c4cf95b901985739a4
ssannkkallpp <ssannkkallpp@gmail.com>
2021-08-01 22:37:15 -0400

    Update data.py
    Fixed data.py to store data as required.

--- data.py
+++ data.py
@@ -1,26 +1,22 @@
-# This file is completed, no need to change.
-# New directory can be created specifically to pickle files into
-# File works as intended but needs to be unit tested.
+# File works as intended but needs to be unit tested. 
 
 from unittest.main import TestProgram
-
-try:
-    import pandas as pd
-    import numpy as np
-    import json
-except ImportError:
-    raise ImportError("Data generation script not importing sufficient dependencies\n")
-# from pathlib import Path
+from numpy.lib.stride_tricks import DummyArray
+import pandas as pd
+import numpy as np 
+import json 
+#from pathlib import Path
 
 
 # Timeframe for the data read is initiated from Jan 22, 2021 : 22/01/2020
-_COVID_CASES_OVER_TIME_DATA = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
-_COVID_DEATHS_OVER_TIME_DATA = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
-_COVID_RECOVERED_OVER_TIME_DATA = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
+_COVID_CASES_OVER_TIME_DATA = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+_COVID_DEATHS_OVER_TIME_DATA = 'https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
+_COVID_RECOVERED_OVER_TIME_DATA = 'https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv'
 
 
-_DATA_PATH = "stored_data"
+_DATA_PATH = "data/"
 
+_FTypes = ["cumulative_total", "cumulative_death", "cumulative_recovered", "incident_total", "incident_death", "incident_recovered"]
 
 class NpEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -34,88 +30,69 @@ class NpEncoder(json.JSONEncoder):
             return super(NpEncoder, self).default(obj)
 
 
+
 def get_incident_cases(array):
     n = len(array)
-    if n == 0:
-        raise ValueError(
-            "Empty list passed to get incident cases from, check fetch_data()\n"
-        )
+    if n == 0: raise ValueError("Empty list passed to get incident cases from, check fetch_data()\n")
     res = [array[0]]
     for i in range(1, len(array)):
-        res.append(array[i] - array[i - 1])
+        res.append(array[i] - array[i-1])
 
     return res
 
 
+
 def fetch_data():
 
-    if (
-        _COVID_CASES_OVER_TIME_DATA
-        or _COVID_DEATHS_OVER_TIME_DATA
-        or _COVID_RECOVERED_OVER_TIME_DATA
-    ) is None:
-        raise AssertionError(
-            "Data links to online csv files not correctly configured\n"
-        )
+    if (_COVID_CASES_OVER_TIME_DATA 
+    or _COVID_DEATHS_OVER_TIME_DATA 
+    or _COVID_RECOVERED_OVER_TIME_DATA) is None:
+        raise AssertionError("Data links to online csv files not correctly configured\n")
 
-    confirmed_df = pd.read_csv(_COVID_CASES_OVER_TIME_DATA, index_col=0)
-    recovered_df = pd.read_csv(_COVID_CASES_OVER_TIME_DATA, index_col=0)
-    death_df = pd.read_csv(_COVID_CASES_OVER_TIME_DATA, index_col=0)
 
-    countries = list(confirmed_df["Country/Region"])
+    print("Loading data from online repo...\n")
+    confirmed_df = pd.read_csv(_COVID_CASES_OVER_TIME_DATA)
+    recovered_df = pd.read_csv(_COVID_CASES_OVER_TIME_DATA)
+    death_df = pd.read_csv(_COVID_CASES_OVER_TIME_DATA)
+    
+    countries = confirmed_df['Country/Region'].unique()
 
-    dates = list(confirmed_df.columns.values)[3:]
-    # print(dates[0])
+    confirmed_df = confirmed_df.drop(columns=['Province/State','Lat', 'Long'])
+    confirmed_df = confirmed_df.groupby('Country/Region').agg('sum')
 
-    # NOTE dates starts on 22nd Jan, 2020
-    c_total_dict = {}
-    r_total_dict = {}
-    d_total_dict = {}
-    c_incident_dict = {}
-    r_incident_dict = {}
-    d_incident_dict = {}
-    for c in range(len(countries)):
-        country = countries[c]
-        total_confirmed = list(confirmed_df.iloc[c][3:])
-        total_death = list(death_df.iloc[c][3:])
-        total_recovered = list(recovered_df.iloc[c][3:])
-        c_total_dict[country] = total_confirmed
-        c_incident_dict[country] = get_incident_cases(total_confirmed)
-        r_total_dict[country] = total_recovered
-        r_incident_dict[country] = get_incident_cases(total_recovered)
-        d_total_dict[country] = total_death
-        d_incident_dict[country] = get_incident_cases(total_death)
 
-    #   predicted_confirmed_cases_dict = {}
-    #   predicted_recovered_cases_dict = {}
-    #   predicted_death_cases_dict = {}
-    #   predicted_incident_cases_dict = {}
-    #   for c in range(len(countries)):
-    #       country = countries[c]
-    #       predicted_confirmed_cases_dict[country] = predict.Predict().predict(confirmed_cases_dict[country], dates)
-    #       predicted_recovered_cases_dict[country] = predict.Predict().predict(recovered_cases_dict[country], dates)
-    #       predicted_death_cases_dict[country] = predict.Predict().predict(death_cases_dict[country], dates)
 
-    data_pickle_file = open(_DATA_PATH, "w")
-    db = {}
-    db["current_total_confirmed"] = c_total_dict
-    db["current_total_recovered"] = r_total_dict
-    db["current_total_deaths"] = d_total_dict
-    db["current_incident_cases"] = c_incident_dict
-    db["current_incident_deaths"] = d_incident_dict
-    db["current_incident_recovered"] = r_incident_dict
-    db["country_list"] = countries
-    db["dates"] = dates
+    recovered_df = recovered_df.drop(columns=['Province/State','Lat', 'Long'])
+    recovered_df = recovered_df.groupby('Country/Region').agg('sum')
 
-    #   db["predicted_confirmed"] = predicted_confirmed_cases_dict
-    #  db["predicted_recovered"] = predicted_recovered_cases_dict
-    #    db["predicted_deaths"] = predicted_death_cases_dict
-    json.dump(db, data_pickle_file, cls=NpEncoder)
 
-    data_pickle_file.close()
+    death_df = death_df.drop(columns=['Province/State','Lat', 'Long'])
+    death_df = death_df.groupby('Country/Region').agg('sum')
 
+    print("Extracted data, now saving to dataset...\n")
+
+    num_countries = len(countries)
+    for c in range(num_countries):
+        country_name = confirmed_df.iloc[c].name 
+        data_table = []
+        data_table.append(list(confirmed_df.iloc[c]))
+        data_table.append(list(death_df.iloc[c]))
+        data_table.append(list(recovered_df.iloc[c]))
+        data_table.append(get_incident_cases(data_table[0]))
+        data_table.append(get_incident_cases(data_table[1]))
+        data_table.append(get_incident_cases(data_table[2]))
+
+        for idx in range(len(_FTypes)):
+            file = open(_DATA_PATH + country_name + "/" + _FTypes[idx], "w")
+            np.savetxt(file, data_table[idx])
+            file.close()
+
+    print("Done loading and saving data set\n")
     return []
+    
+
+    
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     fetch_data()
