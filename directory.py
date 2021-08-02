c29e9a0122bd5e96ed97b01f197218208914ed1c
Lint Action <lint-action@samuelmeuli.com>
2021-08-02 02:02:39 +0000

    Fix code style issues with Black

--- directory.py
+++ directory.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-import numpy as np 
+import numpy as np
 
 _PATH = "data/"
 
@@ -8,13 +8,15 @@ _PATH = "data/"
 print("Setting up data directories ...\n")
 
 
-link = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+link = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 df = pd.read_csv(link)
-countries = df['Country/Region'].unique()
+countries = df["Country/Region"].unique()
 
 
 for c in countries:
-    try: os.mkdir(_PATH + str(c))
-    except FileExistsError: continue
+    try:
+        os.mkdir(_PATH + str(c))
+    except FileExistsError:
+        continue
 
 print("Done\n")
