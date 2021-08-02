85b193b8ad0fb869882c782c39c2ca49a3fb4b67
ssannkkallpp <ssannkkallpp@gmail.com>
2021-08-01 22:02:18 -0400

    Add files via upload
    Added for initial directory creation, but initial data dir will also manually be uploaded to the repo

---
+++ directory.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+import numpy as np 
+
+_PATH = "data/"
+
+
+print("Setting up data directories ...\n")
+
+
+link = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+df = pd.read_csv(link)
+countries = df['Country/Region'].unique()
+
+
+for c in countries:
+    try: os.mkdir(_PATH + str(c))
+    except FileExistsError: continue
+
+print("Done\n")
